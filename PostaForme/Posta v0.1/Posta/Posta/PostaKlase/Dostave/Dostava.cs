7aef459daf06b2d03c02184a9325ee0079cc08d6
BSehic1 <0502996170026Sarajevo>
2016-05-08 18:21:07 +0200

    Dodane neke forme, ostatak ce se dodati kasnije
    Dodane su forme za poslovodju i dostavljace. Sve forme su jos uvijek na
    1280x720 to ce se vjerovatno promijenit iu sljedecem commitu. Onda ima
    jos nekih sitnica(neke kontrole se trebaju pomjeriti malo).

---
+++ PostaForme/Posta v0.1/Posta/Posta/PostaKlase/Dostave/Dostava.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posta
+{
+    class Dostava
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DostavaId { get; set; }
+        public List<Posiljka> listaPosiljaka { get; set; }
+        public List<bool> listaStatusa { get; set; }
+        public Postar dostavljac { get; set; }
+        public Poslovodja poslovodja { get; set; }
+        public DateTime? datumIzdavanja { get; set; }
+
+        public Dostava() { }
+        public Dostava(List<Posiljka> listaPosiljaka, List<bool> listaStatusa, Postar dostavljac, Poslovodja poslovodja, DateTime datumIzdavanja)
+        {
+            this.listaPosiljaka = listaPosiljaka;
+            this.listaStatusa = listaStatusa;
+            this.dostavljac = dostavljac;
+            this.poslovodja = poslovodja;
+            this.datumIzdavanja = datumIzdavanja;
+        }
+    }
+}
---
+++ PostaForme/Posta v0.1/Posta/Posta/PostaKlase/Osobe/Musterije/Posiljalac.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posta
+{
+    class Posiljalac : Osoba
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int posiljalacId { get; set; }
+        public int? brojTelefona { get; set; }
+
+        public Posiljalac() { }
+        public Posiljalac(string ime, string prezime, DateTime datumRodjenja, int brojTelefona) : base(ime,prezime,datumRodjenja)
+        {
+            this.brojTelefona = brojTelefona;
+        }
+    }
+}
