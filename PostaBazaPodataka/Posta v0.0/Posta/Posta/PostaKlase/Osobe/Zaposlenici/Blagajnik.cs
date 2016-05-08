97a019f324eb733ef4de60ef1334aa18b24c6016
BSehic1 <0502996170026Sarajevo>
2016-05-08 11:20:45 +0200

    Folder sa osnovnom bazom podataka
    Baza ima samo podatke, nisu metode dodane

---
+++ PostaBazaPodataka/Posta v0.0/Posta/Posta/PostaKlase/Osobe/Zaposlenici/Blagajnik.cs
@@ -0,0 +1,30 @@
+﻿using Posta.PostaKlase.Zaposlenici;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posta
+{
+    class Blagajnik : Osoba, IZaposlenici
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BlagajnikId { get; set; }
+        public DateTime? datumZaposljenja { get; set; }
+        public int? telefon { get; set; }
+        public string email { get; set; }
+        public string adresa { get; set; }
+
+        public Blagajnik() { }
+        public Blagajnik(string ime, string prezime, DateTime datumRodjenja, DateTime datumZaposljenja, int telefon, string email, string adresa) : base(ime,prezime,datumRodjenja)
+        {
+            this.datumZaposljenja = datumZaposljenja;
+            this.email = email;
+            this.telefon = telefon;
+            this.adresa = adresa;
+        }
+
+    }
+}
---
+++ PostaBazaPodataka/Posta v0.0/Posta/Posta/PostaKlase/Osobe/Zaposlenici/Vozac.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posta
+{
+    class Vozac : Postar
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? VozacId { get; set; }
+        public string brojVozacke { get; set; }
+        public Vozac() { }
+        public Vozac(string ime, string prezime, DateTime datumRodjenja, DateTime datumZaposljenja, int telefon, string email, string adresa,string brojVozacke) : base(ime,prezime,datumRodjenja,datumZaposljenja,telefon,email,adresa)
+        {
+            this.brojVozacke = brojVozacke;
+        }
+    }
+}
