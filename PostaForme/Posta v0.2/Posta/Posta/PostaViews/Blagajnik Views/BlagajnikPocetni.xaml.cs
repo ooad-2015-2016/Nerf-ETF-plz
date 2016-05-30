d81a5e678aeafcc5a6e21f1b0d26b4e42eb69b83
BSehic1 <0502996170026Sarajevo>
2016-05-31 01:46:04 +0200

    Promjena, dodan print button koji kreira privremeni racun svaki put u appdata (Ono gdje se baza nalazi) to je txt fajl btw

--- PostaForme/Posta v0.2/Posta/Posta/PostaViews/Blagajnik Views/BlagajnikPocetni.xaml.cs
+++ PostaForme/Posta v0.2/Posta/Posta/PostaViews/Blagajnik Views/BlagajnikPocetni.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +24,8 @@ namespace Blagajnik
     /// </summary>
     public sealed partial class BlagajnikPocetni : Page
     {
+
+
         public BlagajnikPocetni()
         {
             this.InitializeComponent();
@@ -41,6 +45,118 @@ namespace Blagajnik
         private void LetciRadioButton_Checked(object sender, RoutedEventArgs e)
         {
             DodatneInformacijeFrame.Navigate(typeof(LetciFrame), null);
+        }
+
+        private async void PrintajButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> podaciZaRacun = new List<string>();
+            podaciZaRacun.Add("Posiljalac : " + NazivTextBox.Text + " \r\n");
+            podaciZaRacun.Add("Krajnji rok isporuke : " + KrajnjiRokIsporukeCalenderDatePicker.Date.ToString() + " \r\n");
+            podaciZaRacun.Add("Popust : " + PopustTextBox.Text + "\r\n");
+            
+
+
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile racunSF = await storageFolder.CreateFileAsync("privremeniRacun.txt", CreationCollisionOption.ReplaceExisting);
+
+            var stream = await racunSF.OpenAsync(FileAccessMode.ReadWrite);
+
+            // string racunFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path);
+            // C:\Users\bsehic1\AppData\Local\Packages\32739ef1-3a23-412a-8885-1bf93e9b939e_73k0k8tnbg8ty\LocalState
+
+            
+
+            
+
+
+            if ((bool)PismoRadioButton.IsChecked)
+            {
+                podaciZaRacun.Add("Tip posiljke : Pismo \r\n");
+
+                PismoFrame pf = (PismoFrame)DodatneInformacijeFrame.Content;
+                if (pf != null)
+                {
+                    Grid grid = (Grid)pf.Content;
+                    TextBox PrimalacTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "PrimalacTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Primalac : " + PrimalacTB.Text + "\r\n");
+                    TextBox AdresaTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "AdresaTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Adresa : " + AdresaTB.Text + "\r\n");
+                    
+                }
+
+            }
+
+            if ((bool)PaketRadioButton.IsChecked)
+            {
+                podaciZaRacun.Add("Tip posiljke : Paket \r\n");
+
+                PaketFrame pf = (PaketFrame)DodatneInformacijeFrame.Content;
+                if (pf != null)
+                {
+                    Grid grid = (Grid)pf.Content;
+                    TextBox PrimalacTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "PrimalacTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Primalac : " + PrimalacTB.Text + "\r\n");
+                    TextBox AdresaTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "AdresaTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Adresa : " + AdresaTB.Text + "\r\n");
+
+                    TextBox MasaTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "MasaTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Masa : " + MasaTB.Text + "\r\n");
+                    TextBox VolumenTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "VolumenTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Volumen : " + AdresaTB.Text + "\r\n");
+
+                }
+
+            }
+
+            if ((bool)LetciRadioButton.IsChecked)
+            {
+                podaciZaRacun.Add("Tip posiljke : Letci \r\n");
+
+                LetciFrame pf = (LetciFrame)DodatneInformacijeFrame.Content;
+                if (pf != null)
+                {
+                    Grid grid = (Grid)pf.Content;
+                    TextBox KolicinaTB = (TextBox)grid.Children.Cast<TextBox>().Where(x => x.Name == "KolicinaTextBox").FirstOrDefault();
+                    podaciZaRacun.Add("Kolicina : " + KolicinaTB.Text + "\r\n");
+                    
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            using (var outputStream = stream.GetOutputStreamAt(0))
+            {
+                using (var dataWriter = new Windows.Storage.Streams.DataWriter(outputStream))
+                {
+
+                    for (int i = 0; i < podaciZaRacun.Count; i++)
+                    {
+                        dataWriter.WriteString(podaciZaRacun.ElementAt(i));
+                        
+                    }
+                    await dataWriter.StoreAsync();
+                    await outputStream.FlushAsync();
+                }
+            }
+            stream.Dispose();
+
+
+
+
+
+
+
+
+
         }
     }
 }
--- PostaForme/Posta v0.2/Posta/Posta/PostaViews/Blagajnik Views/PismoFrame.xaml.cs
+++ PostaForme/Posta v0.2/Posta/Posta/PostaViews/Blagajnik Views/PismoFrame.xaml.cs
@@ -26,5 +26,7 @@ namespace Blagajnik
         {
             this.InitializeComponent();
         }
+
+        
     }
 }
